72a09a50c83766f495a64b9f5fb8ea9b03d09507
Samuel Chadwick <samuel.chadwick.software.dev@gmail.com>
2024-02-20 18:29:43 +0100

    First commit

---
+++ GuessTheNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace GuessTheNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 11);
+            int guess = 0;
+
+            Console.WriteLine("Welcome to Guess the Number! Try to guess the randomly generated number. It will be a number between 1 and 10.");
+            do
+            {
+                
+                guess = Convert.ToInt32(Console.ReadLine());
+                if(guess != randomNumber)
+                {
+                    Console.WriteLine("That was not the number, try again!");
+                }
+
+            } while (guess != randomNumber);
+
+            Console.WriteLine("Congrats! You guessed the number. The number was {0}", randomNumber);
+        }
+    }
+}
